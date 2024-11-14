405ff7b0b442e51fec2269ad56f6f4c65eb6e96c
Ronaldo Silva <r9mundo@gmail.com>
2024-11-14 17:09:22 -0400

    task07
    começamos o uso do js

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.mobile_btn').on('click', function(){
+        $('.mobile_menu').toggleClass('active');
+        $('.mobile_btn').find('i').toggleClass('fa-x')
+    })
+})
+
+$(document).ready(function(){
+    $('.mobile_btn').on('click', function(){
+        $('.mobile_menu').toggleClass('active');
+        $('.mobile_btn').find('i').toggleClass('fa-x');
+    });
+});
